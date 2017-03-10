b83e5587f3030dfdbbc83800ed1b70bf431a5a49
Cher <cherbachar08@gmail.com>
2017-03-10 13:47:19 +0000

    kalman filter added

--- Functions/detectPeaks.m
+++ Functions/detectPeaks.m
@@ -48,7 +48,56 @@ df_fixedF0 = ((trace - traceNeuropilSmooth)-min10mat)./min10mat;
 % pause(1);
 
 %% DF/F0 using kalman filter
+kal = 1;
+if kal == 1
+    A = [1.1269   -0.4940    0.1129;
+         1.0000         0         0;
+              0    1.0000         0];
 
+    B = [-0.3832;
+          0.5919;
+          0.5191];
+
+    C = [1 0 0];
+
+    traceKal = ss(trace(17,:));
+    Q = 1;
+    R = 1;
+    [kalmf,L,P,M] = kalman(traceKal,Q,R);
+    kalmf = kalmf(1,:);
+
+    a = A;
+    b = [B B 0*B];
+    c = [C;C];
+    d = [0 0 0;0 0 1];
+    P = ss(a,b,c,d,-1,'inputname',{'u' 'w' 'v'},'outputname',{'y' 'yv'});
+
+    sys = parallel(P,kalmf,1,1,[],[]);
+
+    SimModel = feedback(sys,1,4,2,1);   % Close loop around input #4 and output #2
+    SimModel = SimModel([1 3],[1 2 3]); % Delete yv from I/O list
+
+    t = [0:800]';
+    u = sin(t/5);
+
+    n = length(t);
+    rng default
+    w = sqrt(Q)*randn(n,1);
+    v = sqrt(R)*randn(n,1);
+
+    [out,x] = lsim(SimModel,[w,v,u]);
+
+    y = out(:,1);   % true response
+    ye = out(:,2);  % filtered response
+    yv = y + v;     % measured response
+
+    figure;
+    subplot(211), plot(t,y,'--',t,ye,'-'),
+    xlabel('No. of samples'), ylabel('Output')
+    title('Kalman filter response')
+    subplot(212), plot(t,y-yv,'-.',t,y-ye,'-'),
+    xlabel('No. of samples'), ylabel('Error')
+end
 %% using https://github.com/LeventhalLab/EphysToolbox/tree/master/SpikeySpike
 %http://gaidi.ca/weblog/extracting-spikes-from-neural-electrophysiology-in-matlab
 % for i = 1:numCells
